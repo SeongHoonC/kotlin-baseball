ca1a85a9f954577f75606a30c3dbf334570e6e13
vrexpert <vrexpert2302@gmail.com>
2022-12-07 15:30:41 +0900

    refactor(OutputView): 출력 메세지 하드코딩 제거

--- src/main/kotlin/baseball/view/OutputView.kt
+++ src/main/kotlin/baseball/view/OutputView.kt
@@ -1,15 +1,18 @@
 package baseball.view
 
+import baseball.message.GameMessage.BALL_MESSAGE
+import baseball.message.GameMessage.BALL_STRIKE_MESSAGE
 import baseball.message.GameMessage.NOTHING
+import baseball.message.GameMessage.STRIKE_MESSAGE
 
 class OutputView {
 
     fun printBallStrike(strike: Int, ball: Int) {
         when {
             ball == 0 && strike ==0 -> println(NOTHING)
-            ball > 0 && strike == 0 -> println("${ball}볼")
-            ball == 0 && strike >0 -> println("${strike}스트라이크")
-            ball > 0 && strike >0 -> println("${ball}볼 ${strike}스트라이크")
+            ball > 0 && strike == 0 -> println(BALL_MESSAGE.format(ball))
+            ball == 0 && strike >0 -> println(STRIKE_MESSAGE.format(strike))
+            ball > 0 && strike >0 -> println(BALL_STRIKE_MESSAGE.format(ball,strike))
 
         }
     }
